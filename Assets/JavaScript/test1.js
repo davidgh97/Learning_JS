9a1dcf4d320ee17dc80017e2f01714feb5ae0b43
davidgh97 <124364748+davidgh97@users.noreply.github.com>
2025-07-11 20:47:15 +0300

    Refactor parenthesis validation and array operations
    Simplified the invalidParentheses function in test1.js by removing comments and unnecessary logging, and added a new test case. In test2.js, replaced commented code with examples demonstrating array slice, splice, and toSpliced methods, along with updated console outputs.

--- Assets/JavaScript/test1.js
+++ Assets/JavaScript/test1.js
@@ -1,122 +1,61 @@
 let s = "()[]{}",
-    s2 = "([])[]]{}{]}"
+    s2 = "([])[]]{}{]}",
+    s3 = "([([{()}{}})["
 
 function invalidParenthesis(string) {
-    /*
-    Check if a string full of parathesis is invalid.
-    Rules:
-      1. an open parathesis must close with the first close one encountered
-      2. if a parathesis cannot be closed, then it's invalid
-    Tip: strings can be partitioned into smaller chunks. The first and last characters 
-    are opened, respectively close paranthesis. Going inward, all parathesis should close
-    for all chunks.
-    */
     let correctParenthesis = {
         '(': ')',
         '[': ']',
         '{': '}'
     }
-    
-    // Nothing to work on
+   
+    // check if string is either undefined or null
     if (!string) {
         return 0
     }
 
-    console.log("Working on string: " + string)
-
+    // check that the selected element is always an open paranthesis be it "(", "[", "{"
     let firstChar = string[0]
-    // console.log(Object.values(correctParenthesis))
     if (!(firstChar in correctParenthesis)) {
-        console.log("Couldn't find open paranthesis for: " + firstChar)
-        return 1;
+        return 1
     }
 
-    
+    // find the index of the first open paranthesis that matches our "firstChar"
     let indexEndChar = string.indexOf(correctParenthesis[firstChar])
-    console.log("Found closed paranthesis at " + indexEndChar)
     if (indexEndChar === -1) {
-        console.log("Couldn't find closed paranthesis for: " + firstChar)
-        return 1;
+        return 1
     }
 
+    // check if the current string can be worked on which means checking if there are more than 2 paranthesis in the string 
     if (indexEndChar === 1 && indexEndChar === string.length - 1) {
-        console.log("No more string to work on")
-        return 0;
+        return 0
     }
 
-    // no string ending
-    if (indexEndChar === string.length - 1) {
+    // check if there is an end string -> find middle group
+    if ( indexEndChar === string.length - 1 ) {
         let middle_string = string.slice(1, indexEndChar)
-        console.log("No Ending. Middle string: " + middle_string)
-        return invalidParenthesis(middle_string) + 0
+        return invalidParenthesis(middle_string)
     }
 
-    // no middle string
+    // check if there is no middle string -> find end string
     if (indexEndChar === 1) {
         let end_string = string.slice(indexEndChar + 1, string.length)
-        // console.log("index:" + indexEndChar)
-        console.log("No Middle. End string: " + end_string)
-        return 0 + invalidParenthesis(end_string)
+        return invalidParenthesis(end_string)
     }
 
-    // both middle and end string exist
     let middle_string = string.slice(1, indexEndChar)
     let end_string = string.slice(indexEndChar + 1, string.length)
 
-    console.log("Middle: " + middle_string + " Ending: " + end_string)
     return invalidParenthesis(middle_string) + invalidParenthesis(end_string)
 }
-    
 
 console.log(invalidParenthesis(s))  // good example
 console.log(invalidParenthesis(s2))  // bad example due to '[]]{}{]}'
-console.log(invalidParenthesis("{([])}[]()[[]]"))  // bad example due to '[[]]'
+console.log(invalidParenthesis(s3))  // 
+// console.log(invalidParenthesis("{([])}[]()[[())]]"))  // bad example due to '[[]]'
+ 
     
-    
-    
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+// Some sort of attempt. Maybe will try doing this way too.
     
     
 //     for (let i of string) {
--- Assets/JavaScript/test2.js
+++ Assets/JavaScript/test2.js
@@ -1,38 +1,15 @@
-// let s = "()[]{}",
-//     s2 = "([])[]]{}{]}"
 
-// function invalidParenthesis(string) {
-//     let correctParenthesis = {
-//         '(': ')',
-//         '[': ']',
-//         '{': '}'
-//     }
+let arryy =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],   
+    arryyNames =  ["Diana", "David", "Lilo", "Otto", "Gogu?"],   
+    arryy2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],   
+            //0  1  2  3  4  5  6  7  8  9   10  11  12  13  14
+    trySlice = arryy.slice(5,8),
+    tryToSpliced = arryy.toSpliced(5, 8, "Hello"),
+    trySplice = arryy2.splice(5, 7, arryyNames.slice(1,3))
+        
     
-//     // check if string is either undefined or null
-//     if (!string) {
-//         return 0
-//     }
 
-//     // check that the selected element is always an open paranthesis be it "(", "[", "{"
-//     let firstChar = string[0]
-//     if (!(firstChar in correctParenthesis)) {
-//         return 1
-//     }
-
-//     console.log(firstChar)
-
-//     console.log(firstChar in correctParenthesis)
-
-//     let indexEndChar = string.indexOf(correctParenthesis[firstChar])
-
-//     console.log( indexEndChar) 
-
-// }
-
-// console.log(invalidParenthesis(s2))
-
-
-let arryy = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    bababui = arryy.slice(2,5)
-
-console.log(bababui)
+console.log(arryy) 
+console.log("<--->")
+console.log(trySlice)
+console.log(tryToSpliced) 
